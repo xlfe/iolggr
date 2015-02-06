d569d663dfe0d3037b372de0ace561fe0514fa84
Felix Barbalet <felixb@gmail.com>
2015-02-06 12:09:08 +1100

    Paging based on relative week or start-end dates or most recent observation

--- iolggr/backend.py
+++ iolggr/backend.py
@@ -12,6 +12,7 @@ from google.appengine.ext import ndb
 from models import iot_event,iot_week,format_timestamp
 from datetime import date,datetime,time,timedelta
 
+to_dt = lambda x:datetime.strptime(x, '%Y-%m-%dT%H:%M:%S')
 
 class NDBEncoder(json.JSONEncoder):
     """JSON encoding for NDB models and properties"""
@@ -158,9 +159,54 @@ class device(object):
             if iot_week.query(iot_week.mac == dev_id).fetch(1,keys_only=True) is None:
                 raise AttributeError('No device with MAC {} found'.format(dev_id))
 
+    def _week(self,dt):
+        _dt = iot_week.dt_to_week(dt)
+        return iot_week.query(iot_week.mac == self.dev_id).filter(iot_week.start == _dt).get()
+
+
+    def _weeks(self,start,end):
+
+        assert end > start
+
+        _start = iot_week.dt_to_week(start)
+        weeks = []
+
+        while _start < end:
+            weeks.append(_start)
+            _start += timedelta(days=7)
+
+        weeks = iot_week.query(iot_week.mac == self.dev_id).filter(iot_week.start.IN(weeks)).order(iot_week.start).fetch(10)
+        logging.info('{} weeks retrieved with start {} and end {}'.format(len(weeks),start.isoformat(),end.isoformat()))
+        for week in weeks:
+            logging.info('{} - {} with {} obs'.format(week.start,week.end,len(week.data)))
+
+        #Remove observations outside the start/end pair...
+        if False:
+            o_count =0
+            for week in weeks:
+                outliers = []
+                for k in week.data:
+                    _k = to_dt(k)
+                    if _k > end or _k < start:
+                        outliers.append(k)
+                for o in outliers:
+                    o_count+=1
+                    del week.data[o]
+
+            logging.info('{} outliers removed'.format(o_count))
+        return weeks
+
     def week(self,dt):
-        _week = iot_week.dt_to_week(dt)
-        return iot_week.query(iot_week.mac == self.dev_id).filter(iot_week.start == _week).get()
+        week = self._week(dt)
+        if week is not None:
+            device.clean_week(self.week(dt))
+        return week
+
+    def weeks(self,start,end):
+        weeks = self._weeks(start=start,end=end)
+        for week in weeks:
+            device.clean_week(week)
+        return weeks
 
     @property
     def name(self):
@@ -171,6 +217,16 @@ class device(object):
             older = iot_week.query(projection=['name'], distinct=True).filter(iot_week.mac == self.dev_id).get()
             return older.name
 
+    def prefetch_recent(self):
+        setattr(self,'__recent',iot_event.query(iot_event.mac==self.dev_id).fetch_async(1000))
+
+    def resolve_recent(self):
+        __recent = getattr(self,'__recent',None)
+        assert __recent is not None
+        result = __recent.get_result()
+        setattr(self,'__recent',None)
+        return result
+
     def most_recent_observation(self):
         """
             Get the most recent observation for the device
@@ -178,7 +234,7 @@ class device(object):
         e = iot_event.query(iot_event.mac == self.dev_id).order(-iot_event.timestamp).get()
 
         try:
-            return {
+            return e.name,{
                 format_timestamp(e.timestamp):self.clean_observation(e.params)
             }
 
@@ -189,28 +245,14 @@ class device(object):
             assert len(w.data) > 0
 
             latest = sorted(w.data)[-1]
-            return {
+            return w.name,{
                 latest:self.clean_observation(w.data[latest])
             }
 
-
-    # def get(self,dev_id):
-    #     try:
-    #         results = week.data
-    #         start = week.updated
-    #     except AttributeError:
-    #         results = {}
-    #         start = now - timedelta(hours=24)
-    #
-    #     last_week = week.prev()
-    #     try:
-    #         results.update(last_week.data)
-    #     except AttributeError:
-    #         pass
-    #
-    #     for a in additional_results:
-    #         dt = a.timestamp.replace(microsecond=0).isoformat()
-    #         results[dt]= a.params
+    @staticmethod
+    def clean_week(week):
+        for k in week.data:
+            week.data[k] = device.clean_observation(week.data[k])
 
     @staticmethod
     def clean_observation(result):
@@ -222,8 +264,8 @@ class device_handler(json_response):
         """Get 1 week worth of data plus most recent.
 
         rel: relative to utc.now() - ie 0 is now, -1 is last week, etc
-        mid - time date in UTC for the middle of the week you want
-        nothing - get latest result only
+
+        start, end: get data between
 
         """
         try:
@@ -231,13 +273,67 @@ class device_handler(json_response):
         except AttributeError:
             return self.get_response(404,{})
 
-        r = dev.most_recent_observation()
+
+        rel = self.request.get('rel',None)
+
+        if rel is not None:
+            try:
+                rel = int(rel)
+            except ValueError:
+                return self.get_response(500, {})
+
+            start = datetime.now() - timedelta(days=(rel+1)*7)
+            end   = datetime.now() - timedelta(days=(rel+0)*7)
+
+        else:
+            start = self.request.get('start', None)
+            end = self.request.get('end', None)
+
+            if end is None:
+                end = datetime.now()
+            else:
+                end = to_dt(end)
+
+            if start is not None:
+                start = to_dt(start)
+
+        if start is None:
+            name,result = dev.most_recent_observation()
+        else:
+
+            assert start < datetime.now()
+
+            prefetch = end > datetime.now() - timedelta(hours=3)
+            if prefetch:
+                dev.prefetch_recent()
+                logging.info('Pre-fetching most recent observations')
+
+            weeks = dev.weeks(start=start,end=end)
+            result = {}
+
+            if len(weeks) == 0 and prefetch is False:
+                #No historical data, recent data not requested...
+                return self.get_response(204, {})
+
+            if len(weeks) > 0:
+                name = weeks[-1].name
+
+                for week in weeks:
+                    result.update(week.data)
+
+            if prefetch:
+                recent = dev.resolve_recent()
+                for r in recent:
+                    result[format_timestamp(r.timestamp)] = device.clean_observation(r.params)
+                if len(recent)>0:
+                    name = recent[-1].name
+                logging.info('{} recent results appended'.format(len(recent)))
 
         return self.get_response(200, {
             'device': {
                 'id': dev_id,
-                'name':dev.name,
-                'obs': r
+                'name':name,
+                'obs': result
             }
         })
 
