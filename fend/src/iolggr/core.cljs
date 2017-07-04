bafb8398fa9e4c308762cda96af8ae43181cd91a
Felix Barbalet <felix@pv.tl>
2017-07-05 07:09:17 +1000

    global vs local state...

--- fend/src/iolggr/core.cljs
+++ fend/src/iolggr/core.cljs
@@ -12,26 +12,7 @@
   (:import [goog Uri]
            [goog.net Jsonp]))
 
-
-(defui Chart
-  Object
-  (render [this]
-    (dom/div nil "chart"))
-  (componentDidMount [this]
-    ;    (p/catch #(js/alert (str "Unable to parse spec:\n\n" %)))
-    ;    (p/then #(-> (% {:el (dom/node this)})
-    ;                 (.update))))
-    ))
-
 (enable-console-print!)
-
-(def init-data
-  {:list/one [{:name "John" :points 0}
-              {:name "Mary" :points 0}
-              {:name "Bob" :points 0}]
-   :list/two [{:name "Mary" :points 0 :age 27}
-              {:name "Gwen" :points 0}
-              {:name "Jeff" :points 0}]})
 
 ;; -----------------------------------------------------------------------------
 ;; Parsing
@@ -39,20 +20,14 @@
 ;         name dispatch-fn
 (defmulti read om/dispatch)
 
-(defn get-people [state key]
-  (let [st @state]
-    (into [] (map #(get-in st %)) (get st key))))
-
 ; read function must return a hashmap containing a :value entry
-(defmethod read :list/one
+(defmethod read :sensors
   ; the signature of a read fn is [env key params] - env is a hash map containing context & key is the requested key
   ; destructuring state from env
-  [{:keys [state] :as env} key params]
-  {:value (get-people state key)})
-
-(defmethod read :list/two
-  [{:keys [state] :as env} key params]
-  {:value (get-people state key)})
+  [{:keys [state sensors] :as env} key params]
+  (print "in read :sensors - " @state)
+  {:value (:sensors @state)}
+  )
 
 (defmulti mutate om/dispatch)
 
@@ -66,95 +41,99 @@
             [:person/by-name name :points]
             inc))})
 
-(defmethod mutate 'points/decrement
-  [{:keys [state]} _ {:keys [name]}]
-  {:action
-   (fn []
-     (swap! state update-in
-            [:person/by-name name :points]
-            #(let [n (dec %)] (if (neg? n) 0 n))))})
-
 ;; -----------------------------------------------------------------------------
 ;; Components
 
-(defui Person
-  static om/Ident
-  (ident [this {:keys [name]}]
-    [:person/by-name name])
-  static om/IQuery
-  (query [this]
-    '[:name :points :age])
-  Object
-  (render [this]
-    (let [{:keys [points name] :as props} (om/props this)]
-      (ui/list-item
-        (dom/label nil (str name ", points: " points))
-        (dom/button
-          #js {:onClick
-               (fn [e]
-                 (om/transact! this
-                               `[(points/increment ~props)]))}
-          "+")
-        (dom/button
-          #js {:onClick
-               (fn [e]
-                 (om/transact! this
-                               `[(points/decrement ~props)]))}
-          "-")))
-
-    )
-  )
+;(defui Person
+;  static om/Ident
+;  (ident [this {:keys [name]}]
+;    [:person/by-name name])
+;  static om/IQuery
+;  (query [this]
+;    '[:name :points :age])
+;  Object
+;  (render [this]
+;    (let [{:keys [points name] :as props} (om/props this)]
+;      (ui/list-item
+;        (dom/label nil (str name ", points: " points))
+;        (dom/button
+;          #js {:onClick
+;               (fn [e]
+;                 (om/transact! this
+;                               `[(points/increment ~props)]))}
+;          "+")
+;        (dom/button
+;          #js {:onClick
+;               (fn [e]
+;                 (om/transact! this
+;                               `[(points/decrement ~props)]))}
+;          "-")))
+;
+;    )
+;  )
 ;https://micahasmith.github.io/2015/10/19/clojurescript-is-easy/
 ;https://stackoverflow.com/questions/34810803/how-do-i-use-google-charts-in-clojurescript-with-om
 
-(def person (om/factory Person {:keyfn :name}))
-
-(defui ListView
-  Object
-  (render [this]
-    (let [list (om/props this)]
-      (apply ui/list
-             (map person list)))))
-
-(def list-view (om/factory ListView))
-
-(def sensors [
-              {:name "Laundry"}
-              {:name "Hallway"}
-              {:name "Study"}
-              ]
-  )
+(defonce init-state
+         {
+          :drawer-open? true
+          :sensors     (map #(assoc % :checked true) [
+                         {:mac "1340c89be4c1" :name "Laundry"}
+                         {:mac "1340c89c0a69" :name "Hallway"}
+                         {:mac "1340c89ba2c7" :name "Basement"}
+                         {:mac "1340c89bd3fe" :name "Outside"}
+                         {:mac "1340c89bfd94" :name "Bedroom"}
+                         {:mac "1340c89c2062" :name "Mobile"}
+                         {:mac "1340c89bcd46" :name "Mobile2"}
+                         ])
+          })
 
 
 (defui RootView
+
+  static om/IQuery
+  (query [this]
+    [:sensors]
+    )
+
   Object
   (render [this]
-    (let [{:keys [list/one list/two]} (om/props this)]
+    (let
+      [
+       {:keys [sensors]} (om/props this)
+       {:keys [drawer-open?]} (om/get-state this)
+       ]
       (ui/mui-theme-provider
         {:mui-theme (ui/get-mui-theme)}
         (dom/div
           #js {:className "h-100"}
           (ui/app-bar
-            {:title "iolggr"})
-          (ui/drawer
-             {:open true
-             :docked false
-             :onRequestChange (fn [e] (print e) (.setState this {:open e}))
+            {:title                         "iolggr"
+             :on-left-icon-button-touch-tap #(om/set-state! this {:drawer-open? true})
              }
+            )
+          (ui/drawer
+            {:open              drawer-open?
+             :on-request-change #(om/set-state! this {:drawer-open? %})
+             :docked            false}
             (ui/list
-              (ui/list-item 1)
-              (ui/list-item 1)
-              (ui/list-item 1)
-              (map #(ui/list-item (get % :name)) sensors))
+              (map
+                #(ui/list-item
+                   (ui/checkbox
+                     {
+                      :label         (get % :name)
+                      :labelPosition "left"
+                      :onCheck  #(om/set-state! this {:drawer-open false})
+                      :checked       (get % :checked)}
+                     )
+                ) sensors))
 
-
-
-      ))))))
+            ))))))
 
 
 (def reconciler
   (om/reconciler
-    {:state  init-data
+    {:state  (atom init-state)
      :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler
